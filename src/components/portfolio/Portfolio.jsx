8a90206eb747d2e40c975a5d3b5400aebec93524
Baris Cincik <35776119+baris-cincik@users.noreply.github.com>
2022-01-06 14:40:27 -0500

    about to add bootstra

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,61 +1,57 @@
-import { useEffect, useState } from "react";
-import PortfolioList from "../portfolioList/PortfolioList";
-import "./portfolio.scss";
+import { useEffect, useState } from 'react';
+import PortfolioList from '../portfolioList/PortfolioList';
+import './portfolio.scss';
 import {
   featuredPortfolio,
   webPortfolio,
   mobilePortfolio,
   designPortfolio,
   contentPortfolio,
-} from "../../data";
+} from '../../data';
 
 export default function Portfolio() {
-  const [selected, setSelected] = useState("featured");
+  const [selected, setSelected] = useState('featured');
   const [data, setData] = useState([]);
   const list = [
     {
-      id: "featured",
-      title: "Featured",
+      id: 'featured',
+      title: 'Featured',
     },
     {
-      id: "web",
-      title: "Web App",
+      id: 'web',
+      title: 'Web Apps',
     },
     {
-      id: "mobile",
-      title: "Mobile App",
-    },
-    {
-      id: "design",
-      title: "Design",
-    },
-    {
-      id: "content",
-      title: "Content",
+      id: 'mobile',
+      title: 'Mobile Apps',
     },
   ];
 
   useEffect(() => {
     switch (selected) {
-      case "featured":
+      case 'featured':
         setData(featuredPortfolio);
         break;
-      case "web":
+      case 'web':
         setData(webPortfolio);
         break;
-      case "mobile":
+      case 'mobile':
         setData(mobilePortfolio);
         break;
-      case "design":
+      case 'design':
         setData(designPortfolio);
         break;
-      case "content":
+      case 'content':
         setData(contentPortfolio);
         break;
       default:
         setData(featuredPortfolio);
     }
   }, [selected]);
+
+  function projectClicked(project) {
+    console.log('project clicked');
+  }
 
   return (
     <div className="portfolio" id="portfolio">
@@ -73,10 +69,9 @@ export default function Portfolio() {
       <div className="container">
         {data.map((d) => (
           <div className="item">
-            <img
-              src={d.img}
-              alt=""
-            />
+            <button className="project-button" onClick={projectClicked}>
+              <img src={d.img} alt="" />
+            </button>
             <h3>{d.title}</h3>
           </div>
         ))}
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,25 +1,23 @@
-import "./topbar.scss";
-import { Person, Mail } from "@material-ui/icons";
+import './topbar.scss';
+import { Person, Mail } from '@material-ui/icons';
 
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className={"topbar " + (menuOpen && "active")}>
+    <div className={'topbar ' + (menuOpen && 'active')}>
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">
-            genius.
-          </a>
+          <a className="logo">Portfolio</a>
           <div className="itemContainer">
             <Person className="icon" />
-            <span>+44 924 12 74</span>
+            <span>+1 514 409 87 50</span>
           </div>
           <div className="itemContainer">
             <Mail className="icon" />
-            <span>safak@genius.com</span>
+            <span>baris.cincik@mail.mcgill.ca</span>
           </div>
         </div>
         <div className="right">
-          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
